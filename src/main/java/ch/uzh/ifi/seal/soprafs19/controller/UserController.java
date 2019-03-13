9d179bf257229f23589f9d1240348bdd0312a78a
Degenwitz <dimitri.degkwitz@uzh.ch>
2019-03-13 15:35:28 +0100

    user controller

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
@@ -1,12 +1,16 @@
 package ch.uzh.ifi.seal.soprafs19.controller;
 
+import ch.uzh.ifi.seal.soprafs19.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs19.entity.User;
 import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
 import ch.uzh.ifi.seal.soprafs19.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class UserController {
@@ -18,12 +22,74 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    Iterable<User> all() {
-        return service.getUsers();
+    ResponseEntity all() {
+        Iterable<User> allUsers = service.getUsers();
+        for (User user: allUsers){
+            user.removePassword();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(allUsers);
     }
 
-    @PostMapping("/users")
-    User createUser(@RequestBody User newUser) {
-        return this.service.createUser(newUser);
+    @GetMapping("/users/{id}")
+    ResponseEntity userByID(@PathVariable Long id) {
+        User gef = service.userByID(id);
+        if( gef != null  ) {
+            return ResponseEntity.status(HttpStatus.OK).body(gef.removePassword() );
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("UserName not found");
+        }
     }
+
+    @PostMapping("/login")
+    ResponseEntity getUser(@RequestBody User loginUser) {
+        User savedUser = this.service.userByUsername( loginUser.getUsername() );
+        if( savedUser != null && savedUser.getPassword().equals( loginUser.getPassword() ) ) {
+            this.service.setOnline(savedUser);
+            return ResponseEntity.status(HttpStatus.OK).body(savedUser.removePassword() );
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("UserName or Password wrong");
+        }
+    }
+
+    @GetMapping("/logout/{id}")
+    ResponseEntity logoutUser(@PathVariable Long id) {
+        User testUser = this.service.userByID( id );
+        if( testUser != null) {
+            this.service.setOffline(testUser);
+            return ResponseEntity.status(HttpStatus.OK).body("logged-out");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\n \"message\": \"Username taken\"\n} ");
+        }
+    }
+
+
+    //Used to create new accounts
+    @PostMapping("/users")
+    ResponseEntity createUser(@RequestBody User newUser) {
+        User testUser = this.service.userByUsername( newUser.getUsername() );
+        if( testUser == null) {
+            User createdUser = this.service.createUser(newUser);
+            return ResponseEntity.status(HttpStatus.OK).body( createdUser.removePassword() );
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(" Username already taken ");
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    ResponseEntity userByID(@PathVariable Long id, @RequestBody User newUser) {
+        User gef = service.userByID(id);
+        if( gef == null  ) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body( "user with user-ID: " + id+" not found" );
+        }
+        else{
+            service.updateProfile(newUser, id);
+
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("-");
+        }
+    }
+
 }
