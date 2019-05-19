6d99ab614908172870175c8a312a6745e9eabd30
CarlottaConermann <carlotta@conermann.de>
2019-05-19 09:04:16 +0200

    Update ActionTestGodCards.java

--- src/test/java/ch/uzh/ifi/seal/soprafs19/service/ActionTestGodCards.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs19/service/ActionTestGodCards.java
@@ -426,6 +426,154 @@ public class ActionTestGodCards {
     }
 
     /**
+     * Athena actions test
+     * **/
+
+    @Test
+    public void AthenaActions() throws Exception {
+        //building some structures
+        testGame.getBoard().getSpaces()[1][1].build();
+        testGame.getBoard().getSpaces()[3][2].build();
+
+        //setting the figurines
+        testGame.getStartingPlayer().getFigurine1().setPosition(0, 1);
+        testGame.getStartingPlayer().getFigurine2().setPosition(1, 2);
+        testGame.getNonStartingPlayer().getFigurine1().setPosition(2, 2);
+        testGame.getNonStartingPlayer().getFigurine2().setPosition(3, 3);
+
+        //assign god cards
+        testGame.setStatus(GameStatus.PICKING_GODCARDS);
+        testGame.getStartingPlayer().setAssignedGod(new Athena(testGame));
+        testGame.getNonStartingPlayer().setAssignedGod(new Artemis(testGame));
+        //testGame.checkIfGameOver();
+        testGame.setStatus(GameStatus.MOVING_STARTINGPLAYER);
+
+        gameRepository.save(testGame);
+
+        //get actions
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/game/actions/" + Long.toString(this.player1id)).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/game/actions/" + Long.toString(this.player1id)).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        //making sure we get the right return-code
+        Assert.assertEquals(mvcResult.getResponse().getStatus(), 200);
+
+        //checking content
+        class Response {
+            public int id;
+            public int row;
+            public int column;
+            public int figurine;
+            public boolean withGod;
+
+            Response() {
+            }
+        }
+        ArrayList<Response> actions = new ArrayList<Response>();
+
+        //creating and checking the actions for consistency
+        int i = 0;
+        while (content.charAt(i) != ']') {
+            i += 1;
+            Response response = new Response();
+            Assert.assertEquals("{\"id\":", content.substring(i, i + 6));
+            i += 6;
+            String number = "";
+            while (content.charAt(i) != ',') {
+                number += content.charAt(i);
+                i += 1;
+            }
+            i += 1;
+            response.id = Integer.parseInt(number);
+            if (content.substring(i, i + 30).equals("\"name\":\"movingAsAthena\",\"row\":")) {
+                Assert.assertEquals("\"name\":\"movingAsAthena\",\"row\":", content.substring(i, i + 30));
+                i += 30;
+                i += 30;
+            } else {
+                Assert.assertEquals("\"name\":\"Choose Moving Mode\",\"row\":", content.substring(i, i + 34));
+                i += 34;
+            }
+            number = "";
+            while (content.charAt(i) != ',') {
+                number += content.charAt(i);
+                i += 1;
+            }
+            response.row = Integer.parseInt(number);
+            Assert.assertEquals(",\"column\":", content.substring(i, i + 10));
+            i += 10;
+            number = "";
+            while (content.charAt(i) != ',') {
+                number += content.charAt(i);
+                i += 1;
+            }
+            response.column = Integer.parseInt(number);
+            Assert.assertEquals(",\"figurineNumber\":", content.substring(i, i + 18));
+            i += 18;
+            number = "";
+            while (content.charAt(i) != ',') {
+                number += content.charAt(i);
+                i += 1;
+            }
+            response.figurine = Integer.parseInt(number);
+            Assert.assertEquals(",\"useGod\":", content.substring(i, i + 10));
+            i += 10;
+            if (content.substring(i, i + 4).equals("true")) {
+                i += 5;
+                response.withGod = true;
+            } else {
+                i += 6;
+                response.withGod = false;
+            }
+            actions.add(response);
+        }
+
+        //looking that only allowed moves are here
+        int movements1[][] = new int[5][5];
+        int movements2[][] = new int[5][5];
+
+        Response toPerform = null;
+
+        for (i = 0; i < actions.size(); ++i) {
+            Assert.assertTrue(actions.get(i).figurine == 1 || actions.get(i).figurine == 2);
+            int row = actions.get(i).row;
+            int col = actions.get(i).column;
+            if (actions.get(i).figurine == 1) {
+                movements1[row][col]++;
+            } else if (actions.get(i).figurine == 2) {
+                movements2[row][col]++;
+            }
+            if (actions.get(i).withGod && actions.get(i).withGod && row == 1 && col == 1) {
+                toPerform = actions.get(i);
+            }
+        }
+
+
+        //check if correct movements are saved in movements1 matrix
+        Assert.assertEquals(movements1[0][0], 1);
+        Assert.assertEquals(movements1[0][1], 0);
+        Assert.assertEquals(movements1[0][2], 1);
+        Assert.assertEquals(movements1[1][0], 1);
+        Assert.assertEquals(movements1[1][1], 1); //can move up
+        Assert.assertEquals(movements1[1][2], 0);
+
+        //check if correct movements are saved in movements2 matrix
+        Assert.assertEquals(movements2[0][1], 0);
+        Assert.assertEquals(movements2[0][2], 1);
+        Assert.assertEquals(movements2[0][3], 1);
+        Assert.assertEquals(movements2[1][1], 1); //can move up
+        Assert.assertEquals(movements2[1][2], 0);
+        Assert.assertEquals(movements2[1][3], 1);
+        Assert.assertEquals(movements2[2][1], 1);
+        Assert.assertEquals(movements2[2][2], 0);
+        Assert.assertEquals(movements2[2][3], 1);
+
+        //build to end turn and check if opponent can move up afterwards
+
+    }
+
+
+    /**
      * Atlas actions test
      * **/
 
