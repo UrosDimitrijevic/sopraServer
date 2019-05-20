e5001495562f0bf48c94ff749ffecd064877efa5
Degenwitz <dimitri.degkwitz@uzh.ch>
2019-05-20 13:04:24 +0200

    fixed demeter again, can now always build twice

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/GameController.java
@@ -130,8 +130,8 @@ public class GameController {
         //setting up settings
         game.setPlayWithGodCards(true);
         game.setStatus(GameStatus.MOVING_STARTINGPLAYER);
-        game.getStartingPlayer().setAssignedGod(new Demeter(game));
-        game.getNonStartingPlayer().setAssignedGod(new Apollo(game));
+        game.getStartingPlayer().setAssignedGod(new Artemis(game));
+        game.getNonStartingPlayer().setAssignedGod(new Demeter(game));
 
         //setting up buildings
         game.getBoard().getSpaces()[0][1] = new Space();
