8bf9e6a940582e34ae8b82b19a698bdac6c22b4e
Degenwitz <dimitri.degkwitz@uzh.ch>
2019-05-23 15:11:01 +0200

    removed unneeded part in Prometheus G06-101

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/GodCards/Prometheus.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/GodCards/Prometheus.java
@@ -91,11 +91,7 @@ public class Prometheus extends GodCard {
                 Building build = (Building) simpleBuildings.get(i);
                 if( canMove) {
                     possibleActions.add(new MovingAsPrometheus(game, figurine, build.getRow(), build.getColumn()));
-                } else {
-                    int hight = figurine.retriveSpace().getLevel();
-                    if(game.getBoard().getSpaces()[build.getRow()][build.getColumn()].getLevel() == hight-1){
-                        possibleActions.add(new MovingAsPrometheus(game, figurine, build.getRow(), build.getColumn()));
-                    }
+                }
                 }
             }
         } else if(game.getStatus() == GameStatus.GODMODE_STATE_STARTINGPLAYER || game.getStatus() == GameStatus.GODMODE_STATE_NONSTARTINGPLAYER){
